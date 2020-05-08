e6a443f605e0b84787b6e6304c0c1a62ccf033ea
CocoLB <coco@cocolevant.com>
2020-05-08 14:23:15 -0700

    Challenge completed

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+from sqlalchemy import extract 
 
 # setup Database engine
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -24,17 +25,21 @@ session = Session(engine)
 # setup Flask and create routes
 app = Flask(__name__)
 
+months = ["January", "February", "March", "April", "May", "June", "July",\
+    "August", "September", "October", "November", "December"]
+
 
 @app.route("/")
 def welcome():
     return(
         '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
+    Welcome to the Climate Analysis API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
+    /api/v1.0/temp/start/end<br>
+    /api/v1.0/num_month<br>
     ''')
 
 
@@ -67,6 +72,7 @@ def temp_monthly():
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
+    session = Session(engine)
     sel = [func.min(Measurement.tobs), func.avg(
              Measurement.tobs), func.max(Measurement.tobs)]
     if not end:
@@ -74,12 +80,37 @@ def stats(start=None, end=None):
                    filter(Measurement.date >= start).\
                    filter(Measurement.date <= end).all()
        temps = list(np.ravel(results))
+       session.close()
        return jsonify(temps)
 
     results = session.query(*sel).\
       filter(Measurement.date >= start).\
       filter(Measurement.date <= end).all()
+    session.close()
     temps = list(np.ravel(results))
     return jsonify(temps)
+
+## challenge
+@app.route("/api/v1.0/<nm>")
+def june_stats(nm=1):
+    session = Session(engine)
+    month = months[int(nm)-1]
+    sel1 = [func.min(Measurement.prcp), func.avg(
+             Measurement.prcp), func.max(Measurement.prcp)]
+    sel2 = [func.min(Measurement.tobs), func.avg(
+             Measurement.tobs), func.max(Measurement.tobs)] 
+    prcp_month = session.query(*sel1).filter(extract('month', Measurement.date)==nm).all()
+    tobs_month = session.query(*sel2).filter(extract('month', Measurement.date)==nm).all()
+    session.close()
+    
+    temps1=list(np.ravel(prcp_month))
+    temps2=list(np.ravel(tobs_month))
+       
+    return  jsonify(month, "min-avg-max precipitation", temps1,\
+         "min-avg-max temperature", temps2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
